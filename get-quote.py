8c3d120ccd2f67b802462a3317654f53e2c09cea
logan-e-dutton <74495915+logan-e-dutton@users.noreply.github.com>
2020-11-30 10:53:54 -0500

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,5 +1,5 @@
-def primary():
-   print("Keep it logically awesome.")
+def main():
+  print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
   #quotes = f.readlines()
@@ -7,5 +7,5 @@ def primary():
 
   #print(quotes)
 
-if __name__== "__primary__":
-  primary()
+if __name__ == "__main__":
+  main()
